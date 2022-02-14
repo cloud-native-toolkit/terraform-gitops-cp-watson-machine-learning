f2a40867fd54e5dc37996544053b8995c17f5262
Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>
2022-02-14 17:43:36 +0530

    WML on CPD
    Signed-off-by: Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>

--- test/stages/stage3-cp-wml.tf
+++ test/stages/stage3-cp-wml.tf
@@ -6,6 +6,8 @@ module "gitops_cp_wml" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
-  operator_namespace = module.gitops_cp4d_operator.namespace
-  cpd_namespace = module.gitops_cp4d_instance.namespace
+  #operator_namespace = module.gitops_cp4d_operator.namespace
+  #cpd_namespace = module.gitops_cp4d_instance.namespace
+  operator_namespace = "ibm-common-services"
+  cpd_namespace = "gitops-cp4d-instance"
 }
